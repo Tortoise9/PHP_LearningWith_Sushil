326bcca592f630bb89cb73260b9022543305564e
Sushil Pandey <sushilpandeyy9@gmail.com>
2023-09-28 15:00:56 +0545

    First Commit

---
+++ Day1/Variable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $Name = "sushil pandey";
+    echo "My name is ",$Name;
+    $cast = "pandey";
+    echo " and <br> My surname is ",$cast;
+    $age = 23;
+    print "<br>My age is $age";
+    
+    ?>
+</body>
+</html>
